7132c8e262a2b2d8d165a714773c997a508027de
Keijiro Takahashi <keijiro@gmail.com>
2026-04-05 10:30:03 +0900

    Modified input/output for retake recording

--- Assets/01 Input/Prefilter/Prefilter.cs
+++ Assets/01 Input/Prefilter/Prefilter.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
-using Klak.TestTools;
 using BodyPix;
 
 namespace Karbon {
 
 public sealed class Prefilter : MonoBehaviour
 {
-    [SerializeField] ImageSource _source = null;
+    [SerializeField] RenderTexture _source = null;
     [SerializeField] ResourceSet _resources = null;
     [SerializeField] CustomRenderTexture _target = null;
 
@@ -20,8 +19,8 @@ public sealed class Prefilter : MonoBehaviour
 
     void LateUpdate()
     {
-        _detector.ProcessImage(_source.AsTexture);
-        Shader.SetGlobalTexture(ShaderID.MainTex, _source.AsTexture);
+        _detector.ProcessImage(_source);
+        Shader.SetGlobalTexture(ShaderID.MainTex, _source);
         Shader.SetGlobalTexture(ShaderID.BodyPixTex, _detector.MaskTexture);
         _target.Update();
     }
